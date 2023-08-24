f1d8c02fc9ded563127c94968c78f8ca4dc0191d
Daniel1 <drar17122018@gmail.com>
2023-08-24 19:23:31 +0100

    Añadido

---
+++ src/com/cga/eclipse/HolaMundo.java
@@ -0,0 +1,12 @@
+package com.cga.eclipse;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//git
+		System.out.println("Hola Mundo Git");
+	}
+
+}
---
+++ src/com/cga/eclipse/package-info.java
@@ -0,0 +1,1 @@
+package com.cga.eclipse;
